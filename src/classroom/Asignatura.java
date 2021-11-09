69842d265f53e87adc71e4e5479c6a2c24f2420f
jekamelo <92174216+jekamelo@users.noreply.github.com>
2021-11-09 14:09:29 -0500

    Agregar paquete

---
+++ src/classroom/Asignatura.java
@@ -0,0 +1,48 @@
+package main.java.classroom;
+
+public class Asignatura {
+
+    String nombre;
+    String n = nombre;
+    int codigoInterno;
+    int codigoExterno;
+
+    Asignatura() {
+    	this(0);
+    }
+
+    Asignatura(int codigoInterno) {
+        this("Sin nombre", codigoInterno, codigoInterno);
+    }
+
+    Asignatura(String nombre) {
+        this(nombre, 0, 0);
+    }
+
+    Asignatura(String nombre, int codigoInterno, int codigoExterno) {
+        this.nombre = nombre;
+        this.codigoInterno = codigoInterno;
+        this.codigoExterno = codigoExterno;
+    }
+
+    void cambiarDatos(int codigoInterno, int codigoExterno, String nombre) {
+        this.codigoInterno = codigoInterno;
+        this.codigoExterno = codigoExterno;
+        this.nombre = nombre;
+    }
+
+    void cambiarDatos(int codigo) {
+        this.codigoInterno = codigo;
+        this.codigoExterno = codigo;
+    }
+
+ 
+    void cambiarDatos(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCodigoInterno(int codigoInterno) {
+        this.codigoInterno = codigoInterno;
+    }
+    
+}
---
+++ src/classroom/Persona.java
@@ -0,0 +1,54 @@
+package main.java.classroom;
+
+public class Persona {
+
+    final long cedula ;
+    String nombre;
+    static int totalPersonas;
+    
+    static {
+        totalPersonas = 0;
+ 
+    }
+
+    Persona(long cedula, String nombre) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        totalPersonas++;
+    }
+    
+    Persona(String nombre, long cedula) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        totalPersonas++;
+    }
+
+    Persona(long cedula) {
+        this.cedula = cedula;
+        this.nombre = "";
+        totalPersonas++;
+    }
+
+    Persona(String nombre) {
+        this.nombre = "";
+        this.cedula = 1;
+        totalPersonas++;
+    }
+    Persona(){
+    	this.cedula = 0;
+    	
+    }
+    
+    
+    long getCedula() {
+        return cedula;
+    }
+
+    String getNombre() {
+        return nombre;
+    }
+
+    void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
